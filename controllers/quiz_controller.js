03e1f4f52e2a2facc99306df42042b1015c34646
guarus <johan.alvarado@gmail.com>
2015-08-10 08:17:33 -0430

    busquedas

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -1,13 +1,45 @@
-/* GET /quizes/question */
-exports.question = function(req, res, next) {
-  res.render('quizes/question', { pregunta: 'Capital de Italia' });
+var models = require('../models/models.js');
+
+// Autoload - factoriza el código si la ruta incluye :quizId
+exports.load=function(req, res, next, quizId){
+	models.Quiz.find(quizId).then(function(quiz){
+		if(quiz){	
+			req.quiz = quiz;
+			next();
+		}else{
+			next(new Error('No existe quizId=' + quizId));
+		};
+	}).catch(function(error) {
+				next(error);
+			});	
 };
 
-/* GET /quizes/answer */
-exports.answer = function(req, res, next) {
-  if(req.query.respuesta=='Roma'){
-	res.render('quizes/answer', { respuesta: 'Correcto' });
-  }else{
-	res.render('quizes/answer', { respuesta: 'Incorrecto' });
-  }
+// GET /quizes
+exports.index=function(req, res){
+	var search = '%' + req.query.search + '%';
+	if(search==='%undefined%'){
+		search = search.replace('undefined','');
+	}else{
+		search = search.replace(' ','%');
+	}
+
+	models.Quiz.findAll({where: ["pregunta like ?", search]}).then(function(quizes) {
+		res.render('quizes/index.ejs',{quizes: quizes});
+	}).catch(function(error) {
+				next(error);
+			})		
+};
+
+// GET /quizes/:id
+exports.show=function(req, res){
+	res.render('quizes/show',{quiz: req.quiz});
+};
+
+// GET /quizes/:id/answer
+exports.answer=function(req, res){
+	var resultado = 'Incorrecto';
+	if(req.query.respuesta === req.quiz.respuesta){
+		resultado = 'Correcto';
+	}
+	res.render('quizes/answer',{quiz: req.quiz, respuesta: resultado});
 };
--- routes/index.js
+++ routes/index.js
@@ -8,8 +8,13 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Quiz' });
 });
 
-router.get('/quizes/question', quizController.question);
-router.get('/quizes/answer', quizController.answer);
+// Autoload de comandos con quizId
+router.param('quizId', quizController.load); // autoload :quizId
+
+// Definición de las rutas /quizes
+router.get('/quizes',                      quizController.index);
+router.get('/quizes/:quizId(\\d+)',        quizController.show);
+router.get('/quizes/:quizId(\\d+)/answer', quizController.answer);
 
 /* GET author page. */
 router.get('/author', function(req, res, next) {
